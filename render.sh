9bd53581884e7db7c03815c432cc8a17e48a1230
Daniel Strohbach <d.strohbach@web.de>
2023-03-12 17:34:13 +0100

    Update render.sh

--- render.sh
+++ render.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 day=$(date +%m-%d-%Y)
-relative=/media/timelapse/
+relative=/media/timelapse
 echo "renaming files to be in order"
-find -name '$relative/$day/frames/*.jpg' | gawk 'BEGIN{ a=1 }{ printf "mv %s $relative/$day/frames/%06d.jpg\n", $0, a++$echo "Now converting video (This may take a few minutes...)"
+find -name '*.jpg' | gawk 'BEGIN{ a=1 }{ printf "mv %s $relative/$day/frames/%06d.jpg\n", $0, a++$echo "Now converting video (This may take a few minutes...)"
 gst-launch-1.0 multifilesrc location=$relative/$day/frames/%06d.jpeg index=1 caps="image/jpeg,framerate=24/1" ! jpegdec$echo "Conversion complete"
 echo
 echo
 echo "Cleaning up...."
 sudo rm -rf $relative/$day/frames/
-clear
+#clear
 echo "Your timelapse video is ready! You may view your video at $relative/$day/"
 echo
