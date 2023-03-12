adf73b5ce4a545bf06c9f9875479387d8c648838
Daniel Strohbach <d.strohbach@web.de>
2023-03-12 17:34:39 +0100

    Update timelapse.sh

--- timelapse.sh
+++ timelapse.sh
@@ -7,16 +7,14 @@ mkdir $relative/$day/
 mkdir $relative/$day/frames/
 echo "getting snapshot"
 wget -P $relative/$day/frames http://192.168.178.70:8080/photo.jpg --ignore-length #path to store file and ip-camera sn$mv $relative/$day/frames/photo.jpg $relative/$day/frames/timelapse$(date +-%H%M%S).jpg #using mv command to rename file
-sync
-status=curl http://192.168.178.68/status | jq .Printing
-
+#sync
+status=$(curl 192.168.178.68/status | jq .Printing)
+echo $status
 echo "Printer Status: "
-if [status == true]
+if $status == true
 then
   echo "still printing"
-fi
-if [status == false]
-then
-  sh $relative/render.sh
+else
   echo "printing seems to be done, rendering video"
+  sh $relative/render.sh
 fi
