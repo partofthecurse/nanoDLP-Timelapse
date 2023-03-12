c2d492ca984abdbc685e2aa7878bb3986d4ba850
Daniel Strohbach <d.strohbach@web.de>
2023-03-12 17:41:35 +0100

    Update render.sh

--- render.sh
+++ render.sh
@@ -3,7 +3,7 @@
 day=$(date +%m-%d-%Y)
 relative=/media/timelapse
 echo "renaming files to be in order"
-find -name '*.jpg' | gawk 'BEGIN{ a=1 }{ printf "mv %s $relative/$day/frames/%06d.jpg\n", $0, a++$echo "Now converting video (This may take a few minutes...)"
+find -name '*.jpg' | gawk 'BEGIN{ a=1 }{ printf "mv %s $relative/$day/frames/%06d.jpg\n", $0, a++$echo "Now converting video (This may take a few minutes...)'
 gst-launch-1.0 multifilesrc location=$relative/$day/frames/%06d.jpeg index=1 caps="image/jpeg,framerate=24/1" ! jpegdec$echo "Conversion complete"
 echo
 echo
