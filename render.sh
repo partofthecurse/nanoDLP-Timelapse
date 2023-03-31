15214c483b71442c95cc38bb626ec94cab70f0c4
Daniel Strohbach <d.strohbach@web.de>
2023-04-01 00:50:18 +0200

    Update render.sh
    switched to ffmpeg

--- render.sh
+++ render.sh
@@ -3,12 +3,16 @@
 day=$(date +%m-%d-%Y)
 relative=/media/timelapse
 echo "renaming files to be in order"
-find -name '*.jpg' | gawk 'BEGIN{ a=1 }{ printf "mv %s $relative/$day/frames/%06d.jpg\n", $0, a++$echo "Now converting video (This may take a few minutes...)'
-gst-launch-1.0 multifilesrc location=$relative/$day/frames/%06d.jpeg index=1 caps="image/jpeg,framerate=24/1" ! jpegdec$echo "Conversion complete"
-echo
-echo
-echo "Cleaning up...."
-sudo rm -rf $relative/$day/frames/
+cd $relative/$day/frames
+find . -name '*.jpg'| sort -n | gawk 'BEGIN{ a=0 }{ printf "mv %s %04d.jpg\n", $0, a++ }' | bash
+#gst-launch-1.0 multifilesrc location=$relative/$day/frames/%06d.jpeg index=1 caps="image/jpeg,framerate=24/1" ! jpegdec ! omxh264enc ! avimux ! filesink location=timelapse-$day.avi
+echo "Conversion complete"
+echo "Now converting video (This may take a few minutes...)"
+
+ffmpeg -r 24 -i %04d.jpg -s:v 1920x1080 -c:v libx264 timelapse-$day.mp4
+mv timelapse-$day.mp4 $relative/$day
+#echo "Cleaning up...."
+#sudo rm -rf $relative/$day/frames/
 #clear
-echo "Your timelapse video is ready! You may view your video at $relative/$day/"
-echo
+
+echo "Your timelapse video is ready! You may view your video at" $relative/$day/
